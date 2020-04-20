87051c38f8a3a5fe1abeb457032a4619e23e5ddd
Наталья <Makushina.NA@parma.ru>
2020-04-20 20:49:55 +0500

    as

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,36 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class FirstTest {
+
+
+    WebDriver driver;
+
+    @BeforeTest
+    public void setUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+
+    @Test
+    public void testOtus (){
+
+
+        driver.get("https://otus.ru");
+        System.out.println(driver.getTitle()+"=============");
+
+    }
+
+    @AfterTest
+    public void setDown(){
+        if (driver != null) {
+            driver.quit();
+        }
+
+    }
+}
