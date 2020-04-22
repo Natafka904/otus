e9c41ab34ccf603eac37bc51fa1e1b56daf7c5e8
Наталья <Makushina.NA@parma.ru>
2020-04-22 17:47:12 +0500

    logger

--- src/test/java/FirstTest.java
+++ src/test/java/FirstTest.java
@@ -1,19 +1,26 @@
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
+
+
+
 public class FirstTest {
 
 
     WebDriver driver;
+    private Logger logger = LogManager.getLogger(FirstTest.class);
 
     @BeforeTest
     public void setUp(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        logger.info("Драйвер поднят");
 
     }
 
@@ -23,6 +30,7 @@ public class FirstTest {
 
         driver.get("https://otus.ru");
         System.out.println(driver.getTitle()+"=============");
+        logger.info("Открыт браузер");
 
     }
 
